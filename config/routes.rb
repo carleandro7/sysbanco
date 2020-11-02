f20a4d663ff38af62c843fe9957348dad22b2bff
carleandro7 <carleandro7@gmail.com>
2020-11-02 18:35:50 -0300

    adicionado layout devise e routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accounts , controllers: { 
+    sessions: 'accounts/sessions'
+  }
+  devise_scope :accounts do
+    get 'signin', to: 'accounts/sessions#new'
+    get 'signout', to: 'accounts/registrations#destroy' 
+  end
+  resources :transferaccounts
   resources :transfers
   resources :saques
   resources :depositos
-  resources :accounts
+  resources :accounts, :path => "accounts" do
+    collection do
+      get "/pesquisa"  => "accounts#pesquisa", :as => "pesquisa"
+      get "/menu"  => "accounts#menu", :as => "menu" 
+      get "/encerrarconta"  => "accounts#encerrarconta", :as => "encerrarconta" 
+      get "/extratoconta"  => "accounts#extratoconta", :as => "extratoconta" 
+    end
+  end
+  root "accounts#menu"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
